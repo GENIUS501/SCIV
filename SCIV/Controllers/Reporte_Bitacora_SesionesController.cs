235470d7bc326729f85de32d0235bc9b6b2db349
GENIUS501 <39778070+GENIUS501@users.noreply.github.com>
2020-07-14 15:37:27 -0600

    000

---
+++ SCIV/Controllers/Reporte_Bitacora_SesionesController.cs
@@ -0,0 +1,32 @@
+﻿using SCIV.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SCIV.Controllers
+{
+    public class Reporte_Bitacora_SesionesController : Controller
+    {
+        // GET: Reporte_Bitacora_Sesiones
+        public ActionResult Index()
+        {
+            try
+            {
+                using (SCIVEntities db = new SCIVEntities())
+                {
+                    //Ent_Usuario = (Tab_Usuarios)HttpContext.Session["User"];
+                    List<Tab_Bitacora_Sesiones> lista = db.Tab_Bitacora_Sesiones.ToList();
+                    return View(lista);
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["msg"] = "<script>alert('Error al cargar los datos!!!');</script>";
+                ModelState.AddModelError("Error", ex.Message);
+                return View();
+            }
+        }
+    }
+}
---
+++ SCIV/Controllers/Reporte_MovimientosController.cs
@@ -0,0 +1,32 @@
+﻿using SCIV.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SCIV.Controllers
+{
+    public class Reporte_MovimientosController : Controller
+    {
+        // GET: Reporte_Movimientos
+        public ActionResult Index()
+        {
+            try
+            {
+                using (SCIVEntities db = new SCIVEntities())
+                {
+                    //Ent_Usuario = (Tab_Usuarios)HttpContext.Session["User"];
+                    List<Tab_Bitacora_Movimientos> lista = db.Tab_Bitacora_Movimientos.ToList();
+                    return View(lista);
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["msg"] = "<script>alert('Error al cargar los datos!!!');</script>";
+                ModelState.AddModelError("Error", ex.Message);
+                return View();
+            }
+        }
+    }
+}
